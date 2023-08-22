03ca96638a56c7cccd11f7d0eb1cda60ce95854e
Furhad Jidda <furhadjidda@gmail.com>
2023-08-22 16:04:57 -0500

    Moving robot_bringup package inside src

--- ros2_rpi_ws/src/robot_bringup/launch/robot_base.launch.py
+++ ros2_rpi_ws/src/robot_bringup/launch/robot_base.launch.py
@@ -9,5 +9,10 @@ def generate_launch_description():
         executable = "micro_ros_agent",
         arguments=["serial", "--dev", "/dev/ttyACM0", "-v6"]
     )
+    odometry_tf2_broadcaster = Node(
+        package="astro_odometry_tf_broadcaster",
+        executable = "odometry_tf_broadcaster",
+    )
     ld.add_action(microros_node)
+    ld.add_action(odometry_tf2_broadcaster)
     return ld
