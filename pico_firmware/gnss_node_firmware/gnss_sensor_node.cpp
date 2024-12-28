2267ecdca91dfad3a82974f7b4a42b71d8028344
Furhad <furhadjidda@gmail.com>
2024-12-27 22:23:08 -0600

    Formatting gnss sensor node code

--- pico_firmware/gnss_node_firmware/gnss_sensor_node.cpp
+++ pico_firmware/gnss_node_firmware/gnss_sensor_node.cpp
@@ -15,22 +15,22 @@
  *   along with astro project.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include <rclc/rclc.h>
-#include <rclc/executor.h>
-#include <sensor_msgs/msg/nav_sat_fix.h>
-#include <sensor_msgs/msg/nav_sat_status.h>
-#include <std_msgs/msg/string.h>  // Include this for debug messages
-#include <hardware/uart.h>
-#include <pico/stdlib.h>
-#include <string>
-#include <vector>
-#include <sstream>
-#include <iomanip>
-#include <Adafruit_GPS.hpp>
-#include <rmw_microros/rmw_microros.h>
+#include "gnss_parser.hpp"
 #include "pico/stdlib.h"
 #include "pico_uart_transports.h"
-#include "gnss_parser.hpp"
+#include <Adafruit_GPS.hpp>
+#include <hardware/uart.h>
+#include <iomanip>
+#include <pico/stdlib.h>
+#include <rclc/executor.h>
+#include <rclc/rclc.h>
+#include <rmw_microros/rmw_microros.h>
+#include <sensor_msgs/msg/nav_sat_fix.h>
+#include <sensor_msgs/msg/nav_sat_status.h>
+#include <sstream>
+#include <std_msgs/msg/string.h> // Include this for debug messages
+#include <string>
+#include <vector>
 
 #define UART_ID uart0
 #define BAUD_RATE 9600
@@ -42,10 +42,10 @@ static char nmea_buffer[BUFFER_SIZE];
 static size_t nmea_buffer_index = 0;
 
 rcl_publisher_t publisher;
-rcl_publisher_t dbg_publisher;  // Debug publisher
+rcl_publisher_t dbg_publisher; // Debug publisher
 gnss_parser parser;
 sensor_msgs__msg__NavSatFix nav_sat_fix_msg;
-std::string dbg_msg;  // Message type for debug log
+std::string dbg_msg; // Message type for debug log
 
 // Connect to the GPS on the hardware I2C port
 Adafruit_GPS GPS(i2c0);
@@ -56,14 +56,13 @@ Adafruit_GPS GPS(i2c0);
 uint32_t timer = millis();
 
 void setup() {
-    GPS.Init(0x10);  // The I2C address to use is 0x10
+    GPS.Init(0x10); // The I2C address to use is 0x10
 
     // Set GPS parameters
-    GPS.SendCommand(reinterpret_cast<const uint8_t*>(PMTK_SET_NMEA_OUTPUT_ALLDATA),
+    GPS.SendCommand(reinterpret_cast<const uint8_t *>(PMTK_SET_NMEA_OUTPUT_ALLDATA),
                     strlen(PMTK_SET_NMEA_OUTPUT_ALLDATA));
-    GPS.SendCommand(reinterpret_cast<const uint8_t*>(PMTK_SET_NMEA_UPDATE_1HZ),
-                    strlen(PMTK_SET_NMEA_UPDATE_1HZ));
-    GPS.SendCommand(reinterpret_cast<const uint8_t*>(PGCMD_ANTENNA), strlen(PGCMD_ANTENNA));
+    GPS.SendCommand(reinterpret_cast<const uint8_t *>(PMTK_SET_NMEA_UPDATE_1HZ), strlen(PMTK_SET_NMEA_UPDATE_1HZ));
+    GPS.SendCommand(reinterpret_cast<const uint8_t *>(PGCMD_ANTENNA), strlen(PGCMD_ANTENNA));
     sleep_ms(1000);
 }
 
@@ -78,24 +77,18 @@ void loop() {
 
         // Publish debug message
         std::string fix = (GPS.mFix) ? "true" : "false";
-        dbg_msg = std::string("Received NMEA sentence: " + nmea_sentence + " >>> Fix " + fix );  // Set debug message
-        rcl_publish(&dbg_publisher, &dbg_msg, NULL);  // Publish to dbg_mesg topic
+        dbg_msg = std::string("Received NMEA sentence: " + nmea_sentence + " >>> Fix " + fix); // Set debug message
+        rcl_publish(&dbg_publisher, &dbg_msg, NULL); // Publish to dbg_mesg topic
 
-
-        nav_sat_fix_msg = parser.packData(GPS.mLatitude,GPS.mLat, GPS.mLongitude,GPS.mLon ,GPS.mAltitude, GPS.mFix,GPS.mFixquality_3d,GPS.mHDOP,GPS.mPDOP,GPS.mVDOP );
+        nav_sat_fix_msg = parser.packData(GPS.mLatitude, GPS.mLat, GPS.mLongitude, GPS.mLon, GPS.mAltitude, GPS.mFix,
+                                          GPS.mFixquality_3d, GPS.mHDOP, GPS.mPDOP, GPS.mVDOP);
         rcl_publish(&publisher, &nav_sat_fix_msg, NULL);
     }
 }
 
 int main() {
-    rmw_uros_set_custom_transport(
-        true,
-        NULL,
-        pico_serial_transport_open,
-        pico_serial_transport_close,
-        pico_serial_transport_write,
-        pico_serial_transport_read
-    );
+    rmw_uros_set_custom_transport(true, NULL, pico_serial_transport_open, pico_serial_transport_close,
+                                  pico_serial_transport_write, pico_serial_transport_read);
 
     setup();
 
@@ -107,8 +100,10 @@ int main() {
     rcl_node_t node;
     rclc_node_init_default(&node, "gnss_publisher", "", &support);
 
-    rclc_publisher_init_default(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, NavSatFix), "gnss_data");
-    rclc_publisher_init_default(&dbg_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String), "dbg_mesg");  // Initialize debug publisher
+    rclc_publisher_init_default(&publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(sensor_msgs, msg, NavSatFix),
+                                "gnss_data");
+    rclc_publisher_init_default(&dbg_publisher, &node, ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, String),
+                                "dbg_mesg"); // Initialize debug publisher
 
     rclc_executor_t executor;
     rclc_executor_init(&executor, &support.context, 1, &allocator);
@@ -122,7 +117,7 @@ int main() {
 
     // Cleanup
     rcl_publisher_fini(&publisher, &node);
-    rcl_publisher_fini(&dbg_publisher, &node);  // Cleanup debug publisher
+    rcl_publisher_fini(&dbg_publisher, &node); // Cleanup debug publisher
     rcl_node_fini(&node);
     rclc_support_fini(&support);
 
--- pico_firmware/gnss_node_firmware/pico_uart_transport.c
+++ pico_firmware/gnss_node_firmware/pico_uart_transport.c
@@ -1,29 +1,23 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-#include <stdio.h>
 #include "pico/stdlib.h"
+#include <stdio.h>
 #include <uxr/client/profile/transport/custom/custom_transport.h>
 
-void usleep(uint64_t us)
-{
-    sleep_us(us);
-}
+void usleep(uint64_t us) { sleep_us(us); }
 
-int clock_gettime(clockid_t unused, struct timespec *tp)
-{
+int clock_gettime(clockid_t unused, struct timespec *tp) {
     uint64_t m = time_us_64();
     tp->tv_sec = m / 1000000;
     tp->tv_nsec = (m % 1000000) * 1000;
     return 0;
 }
 
-bool pico_serial_transport_open(struct uxrCustomTransport * transport)
-{
+bool pico_serial_transport_open(struct uxrCustomTransport *transport) {
     // Ensure that stdio_init_all is only called once on the runtime
     static bool require_init = true;
-    if(require_init)
-    {
+    if (require_init) {
         stdio_init_all();
         require_init = false;
     }
@@ -31,17 +25,11 @@ bool pico_serial_transport_open(struct uxrCustomTransport * transport)
     return true;
 }
 
-bool pico_serial_transport_close(struct uxrCustomTransport * transport)
-{
-    return true;
-}
+bool pico_serial_transport_close(struct uxrCustomTransport *transport) { return true; }
 
-size_t pico_serial_transport_write(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, uint8_t *errcode)
-{
-    for (size_t i = 0; i < len; i++)
-    {
-        if (buf[i] != putchar(buf[i]))
-        {
+size_t pico_serial_transport_write(struct uxrCustomTransport *transport, uint8_t *buf, size_t len, uint8_t *errcode) {
+    for (size_t i = 0; i < len; i++) {
+        if (buf[i] != putchar(buf[i])) {
             *errcode = 1;
             return i;
         }
@@ -49,21 +37,18 @@ size_t pico_serial_transport_write(struct uxrCustomTransport * transport, uint8_
     return len;
 }
 
-size_t pico_serial_transport_read(struct uxrCustomTransport * transport, uint8_t *buf, size_t len, int timeout, uint8_t *errcode)
-{
+size_t pico_serial_transport_read(struct uxrCustomTransport *transport, uint8_t *buf, size_t len, int timeout,
+                                  uint8_t *errcode) {
     uint64_t start_time_us = time_us_64();
-    for (size_t i = 0; i < len; i++)
-    {
+    for (size_t i = 0; i < len; i++) {
         int64_t elapsed_time_us = timeout * 1000 - (time_us_64() - start_time_us);
-        if (elapsed_time_us < 0)
-        {
+        if (elapsed_time_us < 0) {
             *errcode = 1;
             return i;
         }
 
         int character = getchar_timeout_us(elapsed_time_us);
-        if (character == PICO_ERROR_TIMEOUT)
-        {
+        if (character == PICO_ERROR_TIMEOUT) {
             *errcode = 1;
             return i;
         }
@@ -71,7 +56,6 @@ size_t pico_serial_transport_read(struct uxrCustomTransport * transport, uint8_t
     }
     return len;
 }
-
 
 #ifdef __cplusplus
 }
