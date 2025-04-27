4e01a8e26092d0a88a83b6bbcd05dfc354e97f7f
Furhad <furhadjidda@gmail.com>
2025-04-27 11:11:31 -0500

    Orienting the odometer and lidar correctly with the map

--- ros2_ws/src/robot_bringup/launch/robot_base.launch.py
+++ ros2_ws/src/robot_bringup/launch/robot_base.launch.py
@@ -95,12 +95,12 @@ def generate_launch_description():
     base_link_to_imu_link = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "base_link", "imu_link"],
+        arguments=["0", "0", "0", "0", "3.1416", "0", "base_link", "imu_link"],
     )
     base_link_to_laser_link = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
-        arguments=["0", "0", "0", "0", "0", "0", "base_link", "laser"],
+        arguments=["0", "0", "0", "0", "3.1416", "0", "base_link", "laser"],
     )
     base_link_to_camera_link = Node(
         package="tf2_ros",
