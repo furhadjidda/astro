437c95c8790842c056ba2ae8aa318c04a258c284
Furhad <furhadjidda@gmail.com>
2026-02-03 17:07:33 -0600

    update the rak14001.c example to include more colors

--- zephyr_workspace/example_apps/examples/rak14001.c
+++ zephyr_workspace/example_apps/examples/rak14001.c
@@ -19,7 +19,9 @@ int main(void) {
     /* Get LED device */
     led_dev = DEVICE_DT_GET(LED_DEV_NODE);
 
-    static uint8_t rgb_colors[3] = {255, 255, 0};
+    static uint8_t rgb_colors1[3] = {255, 255, 0};
+    static uint8_t rgb_colors2[3] = {255, 255, 255};
+    static uint8_t rgb_colors3[3] = {0, 255, 255};
 
     if (!device_is_ready(led_dev)) {
         LOG_ERR("LED device %s not ready", led_dev->name);
@@ -29,7 +31,7 @@ int main(void) {
     LOG_INF("LED device %s is ready", led_dev->name);
 
     /* CRITICAL: Enable the LED first with brightness */
-    ret = led_set_color(led_dev, 0, 3, rgb_colors);
+    ret = led_set_color(led_dev, 0, 3, rgb_colors1);
     if (ret) {
         LOG_ERR("Failed to set color: %d", ret);
         return ret;
@@ -38,12 +40,15 @@ int main(void) {
     while (1) {
         /* Red */
         LOG_INF("Turning LED ON (all channels)");
-        // ret = led_set_color(led_dev, 0, 3, rgb_colors);
-
-        // if (ret) {
-        //     LOG_ERR("Failed to set color: %d", ret);
-        //     return ret;
-        // }
+        ret = led_set_color(led_dev, 0, 3, rgb_colors2);
+        k_sleep(K_MSEC(500));        
+        ret = led_set_color(led_dev, 0, 3, rgb_colors3);
+        k_sleep(K_MSEC(500));        
+        ret = led_set_color(led_dev, 0, 3, rgb_colors1);
+        if (ret) {
+            LOG_ERR("Failed to set color: %d", ret);
+            return ret;
+        }
         k_sleep(K_MSEC(1000));
     }
 
